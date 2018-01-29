17ad0391736993c1eec002f0e1765152dcb8048e
Markel Tuzinskiy <mtuzinskiy@klika-tech.com>
2018-01-29 16:17:30 +0200

    Fix the Map keys for case with multiple ObservableInputs

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,24 @@
 import { ReplaySubject } from 'rxjs/ReplaySubject'
 
-const subjects = new WeakMap()
+const subjects = new Map();
 
 export function ObservableInput() {
   return (target, propertyKey) => {
-    delete target[propertyKey]
+    delete target[propertyKey];
 
     Object.defineProperty(target, propertyKey, {
       set(value) {
-        this[propertyKey].next(value)
+        this[propertyKey].next(value);
       },
+
       get() {
-        let subject = subjects.get(this)
-        if (! subject)  {
-          subject = new ReplaySubject<any>(1)
-          subjects.set(this, subject)
+        let subject = subjects.get(propertyKey);
+        if (!subject)  {
+          subject = new ReplaySubject<any>(1);
+          subjects.set(propertyKey, subject);
         }
-        return subject
-      },
-    })
+        return subject;
+      }
+    });
   }
 }
