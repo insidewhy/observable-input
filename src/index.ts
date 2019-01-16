4d6bcd1d51548552468cedf61bfac60f3d3b260d
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-01-16 17:28:46 +0100

    reuse returned observables

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,41 @@
-import { ReplaySubject } from 'rxjs'
+import { Observable, ReplaySubject } from 'rxjs'
 
+type ObservableByProp = Map<string, Observable<any>>
 type SubjectByProp = Map<string, ReplaySubject<any>>
 
+const observables: WeakMap<Object, ObservableByProp> = new WeakMap()
 const subjects: WeakMap<Object, SubjectByProp> = new WeakMap()
 
-const getSubject = (instance: any, propertyKey: string): ReplaySubject<any> => {
-  const subjectByProp: SubjectByProp = subjects.get(instance) || new Map()
-  let subject = subjectByProp.get(propertyKey)
+const getMap = <T> (weakMap: WeakMap<Object, Map<string, T>>, instance: Object): Map<string, T> => {
+  const map: Map<string, T> = weakMap.get(instance)
+  if (map) {
+    return map
+  }
+  const newMap: Map<string, T> = new Map()
+  weakMap.set(instance, newMap)
+  return newMap
+}
+
+const getSubject = (instance: Object, propertyKey: string): ReplaySubject<any> => {
+  const subjectByProp = getMap(subjects, instance)
+  const subject = subjectByProp.get(propertyKey)
   if (subject) {
     return subject
   }
-  subject = new ReplaySubject<any>(1)
-  subjectByProp.set(propertyKey, subject)
-  subjects.set(instance, subjectByProp)
-  return subject
+  const newSubject = new ReplaySubject<any>(1)
+  subjectByProp.set(propertyKey, newSubject)
+  return newSubject
+}
+
+const getObservable = (instance: Object, propertyKey: string): Observable<any> => {
+  const observableByProp = getMap(observables, instance)
+  const observable = observableByProp.get(propertyKey)
+  if (observable) {
+    return observable
+  }
+  const newObservable = getSubject(instance, propertyKey).asObservable()
+  observableByProp.set(propertyKey, newObservable)
+  return newObservable
 }
 
 export function ObservableInput() {
@@ -22,12 +44,10 @@ export function ObservableInput() {
 
     Object.defineProperty(target, propertyKey, {
       set(value) {
-        const subject = getSubject(this, propertyKey)
-        subject.next(value)
+        getSubject(this, propertyKey).next(value)
       },
       get() {
-        const subject = getSubject(this, propertyKey)
-        return subject.asObservable()
+        return getObservable(this, propertyKey)
       },
     })
   }
