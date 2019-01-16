bcfbf031763a95f3f769f521a9240a9b403cf609
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-01-16 17:08:33 +0100

    fix setter by using the underlying subject

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,29 @@ type SubjectByProp = Map<string, ReplaySubject<any>>
 
 const subjects: WeakMap<Object, SubjectByProp> = new WeakMap()
 
+const getSubject = (instance: any, propertyKey: string): ReplaySubject<any> => {
+  const subjectByProp: SubjectByProp = subjects.get(instance) || new Map()
+  let subject = subjectByProp.get(propertyKey)
+  if (subject) {
+    return subject
+  }
+  subject = new ReplaySubject<any>(1)
+  subjectByProp.set(propertyKey, subject)
+  subjects.set(instance, subjectByProp)
+  return subject
+}
+
 export function ObservableInput() {
   return (target, propertyKey) => {
     delete target[propertyKey]
 
     Object.defineProperty(target, propertyKey, {
       set(value) {
-        this[propertyKey].next(value)
+        const subject = getSubject(this, propertyKey)
+        subject.next(value)
       },
       get() {
-        const subjectByProp: SubjectByProp = subjects.get(this) || new Map()
-        let subject = subjectByProp.get(propertyKey)
-        if (! subject)  {
-          subject = new ReplaySubject<any>(1)
-          subjectByProp.set(propertyKey, subject)
-          subjects.set(this, subjectByProp)
-        }
+        const subject = getSubject(this, propertyKey)
         return subject.asObservable()
       },
     })
