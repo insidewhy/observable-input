insidewhy/observable-input

commit 68029ae79816cb27918a4f76ac42881a4a38517f

Return observable (#8)
The library should not expose the Subject, but instead return an Observable, using [asObservable](https://rxjs-dev.firebaseapp.com/api/index/class/Subject#asobservable-).

src/index.ts
@@ -20,7 +20,7 @@ export function ObservableInput() {
           subjectByProp.set(propertyKey, subject)
           subjects.set(this, subjectByProp)
         }
-        return subject
+        return subject.asObservable()
       },
     })
   }

commit 37bbd220900ed5436c416bca0d362a263d91ed24

fix code broken by 68029ae

src/index.ts
@@ -1,8 +1,31 @@
-import { ReplaySubject } from 'rxjs'
+import { Observable, ReplaySubject } from 'rxjs'
 
-type SubjectByProp = Map<string, ReplaySubject<any>>
+interface SubjectAndObservable { observable: Observable<any>; subject: ReplaySubject<any> }
+type ComponentSubjectsAndObservables = Map<string, SubjectAndObservable>
 
-const subjects: WeakMap<Object, SubjectByProp> = new WeakMap()
+const subjectsAndObservables = new WeakMap<Object, ComponentSubjectsAndObservables>()
+
+const getComponentSubjectsAndObservables = (instance: Object): ComponentSubjectsAndObservables => {
+  const componentSubjectsAndObservables: ComponentSubjectsAndObservables = subjectsAndObservables.get(instance)
+  if (componentSubjectsAndObservables) {
+    return componentSubjectsAndObservables
+  }
+  const newComponentSubjectsAndObservables: ComponentSubjectsAndObservables = new Map()
+  subjectsAndObservables.set(instance, newComponentSubjectsAndObservables)
+  return newComponentSubjectsAndObservables
+}
+
+const getSubjectAndObservable = (instance: Object, propertyKey: string): SubjectAndObservable => {
+  const componentSubjectsAndObservables = getComponentSubjectsAndObservables(instance)
+  const subjectAndObservable = componentSubjectsAndObservables.get(propertyKey)
+  if (subjectAndObservable) {
+    return subjectAndObservable
+  }
+  const subject = new ReplaySubject<any>(1)
+  const newSubjectAndObservable = { observable: subject.asObservable(), subject }
+  componentSubjectsAndObservables.set(propertyKey, newSubjectAndObservable)
+  return newSubjectAndObservable
+}
 
 export function ObservableInput() {
   return (target, propertyKey) => {
@@ -10,17 +33,10 @@ export function ObservableInput() {
 
     Object.defineProperty(target, propertyKey, {
       set(value) {
-        this[propertyKey].next(value)
+        getSubjectAndObservable(this, propertyKey).subject.next(value)
       },
       get() {
-        const subjectByProp: SubjectByProp = subjects.get(this) || new Map()
-        let subject = subjectByProp.get(propertyKey)
-        if (! subject)  {
-          subject = new ReplaySubject<any>(1)
-          subjectByProp.set(propertyKey, subject)
-          subjects.set(this, subjectByProp)
-        }
-        return subject.asObservable()
+        return getSubjectAndObservable(this, propertyKey).observable
       },
     })
   }